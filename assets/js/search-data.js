1c91e6ddae19e8fea14c526df73872e3c96db1d3
PyHackSecGP <gpsingh2796@protonmail.com>
2026-06-27 22:17:27 +0000

    Deploying to gh-pages from @ PyHackSecGP/pyhacksecgp.github.io@834553c3712bc9ee3f83922956165a2ecc9a745d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-what-i-m-learning-from-the-4-hour-workweek-and-why-it-s-blowing-my-mind",
         
           title: "What I’m Learning from The 4-Hour Workweek (And Why It’s Blowing My Mind)...",
